5d8a3f0614cefc78941fa71f01f31ec513c2381e
Valentin Besse <valentinbesse@aumbox.net>
2025-04-22 16:37:16 +0200

    Add views for articles' list and detail of an article

--- blog/articles/views.py
+++ blog/articles/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Article
 
 # Create your views here.
+def list_articles(request):
+    articles = Article.objects.all()
+    return render(request, 'articles/list_articles.html', {'articles': articles})
+
+def detail_article(request, article_id):
+    article = get_object_or_404(Article, pk=article_id)
+    return render(request, 'articles/detail_article.html', {'article': article})
