554ef68abce62dfb9e9a383f0787377f796cbf7a
Valentin Besse <valentinbesse@aumbox.net>
2025-04-22 16:50:18 +0200

    Modify view and tempalt to add comment

--- blog/articles/views.py
+++ blog/articles/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Article
+from .forms import CommentForm
 
 # Create your views here.
 def list_articles(request):
@@ -8,4 +9,13 @@ def list_articles(request):
 
 def detail_article(request, article_id):
     article = get_object_or_404(Article, pk=article_id)
-    return render(request, 'articles/detail_article.html', {'article': article})
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.article = article
+            comment.save()
+            return redirect('detail_article', article_id=article.id)
+    else:
+        form = CommentForm()
+    return render(request, 'articles/detail_article.html', {'article': article, 'form': form})
