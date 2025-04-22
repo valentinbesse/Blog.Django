5f8a5c207bd333fdfe35b42c20812e23aa815ad2
Valentin Besse <valentinbesse@aumbox.net>
2025-04-22 16:28:44 +0200

    Add model for article

--- blog/articles/models.py
+++ blog/articles/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    publication_date = models.DateField()
+    last_updated = models.DateTimeField(auto_now=True)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Comment(models.Model):
+    article = models.ForeignKey(Article, related_name="comments", on_delete=models.CASCADE)
+    author = models.CharField(max_length=100)
+    content = models.TextField()
+    published_date = models.DateField()
+    last_updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'Comment by {self.author} on {self.published_date}'
